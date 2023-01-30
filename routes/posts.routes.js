5e88d066f8834deb935672dbfb2393513e389171
Matundu Jules-Langa <matundu.pro@gmail.com>
2023-01-30 16:52:53 +0100

    create route for delete post

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -2,6 +2,7 @@ const {
     postCreate,
     postGetAll,
     postForm,
+    postDelete,
 } = require('../controllers/posts.controller')
 
 const router = require('express').Router()
@@ -12,5 +13,6 @@ router.get('/new', postForm)
 
 // API
 router.post('/', postCreate)
+router.delete('/:postId', postDelete)
 
 module.exports = router
